752c34b7037d53b9994815788312843f977a5c35
KayLeb <calebogbike@gmail.com>
2022-05-14 17:31:01 +0100

    Initial Commit

---
+++ portfolio.js
@@ -0,0 +1,26 @@
+const colors = ['red', 'green', 'blue', 'purple']
+const heading = document.querySelector('header')
+const heads = document.querySelectorAll('h1')
+
+heading.addEventListener('click', function(){
+    let randomColor = getRandomColor()
+    heading.style.backgroundColor = colors[randomColor]
+    heading.style.transition = '1s'
+
+})
+
+function getRandomColor() {
+    return Math.floor(Math.random()*colors.length)
+}
+
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+})
+ScrollReveal().reveal('.hi-text', {delay: 500})
+ScrollReveal().reveal('h1', {delay: 400})
+ScrollReveal().reveal('img', {delay: 500})
+ScrollReveal().reveal('i', {delay: 400})
+
